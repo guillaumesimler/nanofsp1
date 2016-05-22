d282cf480c27cc4d2e0b9f909dd3b4433a40ec16
GuillaumeS <guillaume.simler@gmail.com>
2016-05-22 14:51:58 +0200

    Let's go

---
+++ README.md
@@ -0,0 +1,50 @@
+# Movie Web Trailer Website
+
+Authors
+----
+* the [initial program](https://github.com/adarsh0806/ud036_StarterCode) was designed by the [udacity team under the lead of Adarsh Nair](https://github.com/adarsh0806)
+
+* the **adaptation** was done by **Guillaume Simler**, a Udacity Frontend Nanodegree graduate and Full Stack Web Developer Student, more information and contact details on my [Github profile](https://github.com/guillaumesimler)
+
+Project description
+----
+
+Most of the works were already done. There are adaptation done by Guillaume Simler, especially
+
+Used resources
+----
+
+### Libraries & frameworks (html, css, js)
+
+There were no additions from Guillaume Simler, all were preexistents
+
+* [**Bootstrap**](http://getbootstrap.com/), both __css__ and __javascript__ version
+* [**jquery**](https://jquery.com/)
+
+
+### APIs
+
+
+
+How to use
+----
+
+#### Initialize the project
+
+1. Clone the [Repo](https://github.com/guillaumesimler/nanofsp1)
+2. Open **entertainement_center.py** and run the module
+3. Enjoy 
+
+#### Later on 
+
+* launch **fresh_tomatoes.html**
+
+
+Repository 
+----
+* the [working project](https://github.com/guillaumesimler/nanofsp1)
+
+License
+----
+
+The **current version** is under [_MIT License_](https://github.com/guillaumesimler/nanofsp1/blob/master/LICENSE.txt) 
---
+++ entertainment_center.py
@@ -0,0 +1,41 @@
+import media
+import fresh_tomatoes
+
+apocalypse_now = media.Movie("Apocalypse Now",
+                             "Let's kill a colonel, sorry terminate his commands",
+                             "https://upload.wikimedia.org/wikipedia/en/c/c2/Apocalypse_Now_poster.jpg",
+                             "https://www.youtube.com/watch?v=bPXVGQnJm0w")
+
+english_patient = media.Movie("The English Patient",
+                             "A remembrance of a love story at the end of WWII",
+                             "https://upload.wikimedia.org/wikipedia/en/b/bd/The_English_Patient_Poster.jpg",
+                             "https://www.youtube.com/watch?v=WPKpU14Kqqw")
+
+
+inception = media.Movie("Inception",
+                        "Using the full power of subconsciousness and dreams and changing it",
+                        "https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
+                        "https://www.youtube.com/watch?v=d3A3-zSOBT4")
+
+je_vais_bien = media.Movie("Je vais bien, ne t'en fais pas",
+                           "Searching for the dead twin brother",
+                           "https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Je_vais_bien%2C_ne_t%27en_fais_pas_poster.jpg/220px-Je_vais_bien%2C_ne_t%27en_fais_pas_poster.jpg",
+                           "https://www.youtube.com/watch?v=wJRh0PlWB6g")
+
+
+oceans_11 = media.Movie("Ocean's 11",
+                        "Let's win it all: the money, revenge and ... the girl",
+                        "https://upload.wikimedia.org/wikipedia/en/6/68/Ocean%27s_Eleven_2001_Poster.jpg",
+                        "https://www.youtube.com/watch?v=imm6OR605UI")
+
+v_for_vendetta= media.Movie("V for Vendetta",
+                            "It is more than a personal vendetta",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Vforvendettamov.jpg/220px-Vforvendettamov.jpg",
+                            "https://www.youtube.com/watch?v=qxyUl9M_7vc")
+
+
+movies = [apocalypse_now, english_patient, inception, je_vais_bien, oceans_11, v_for_vendetta]
+
+fresh_tomatoes.open_movies_page(movies)                     
+
+
